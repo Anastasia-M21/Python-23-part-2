840b0be8be2f27def0aaf9648ea39bbf07418967
Anastasia-M21 <144236415+Anastasia-M21@users.noreply.github.com>
2024-01-19 00:05:35 +0300

    Update task 2.py

--- Лабораторная 2/Лаба 2/task 2.py
+++ Лабораторная 2/Лаба 2/task 2.py
@@ -20,12 +20,6 @@ class Book:
         self.name = name
         self.pages = pages
 
-    def __str__(self) -> str:
-        return f'Книга "{self.name}"'
-
-    def __repr__(self) -> str:
-        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
-
 # TODO написать класс Library
 class Library:
     def __init__(self, books: Optional[List[Book]] = None):
